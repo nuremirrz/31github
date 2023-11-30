6f8b2dabfed5c1a7768e05f9d45551f1c39f58aa
Emirlan <nur.emirlan.02@gmail.com>
2023-11-30 13:38:10 +0600

    30

---
+++ 30.11.2023/index.js
@@ -0,0 +1,9 @@
+let scoreDolphins = (96+108+89)/3
+let scoreKoalas = (108+89+96)/3
+if(scoreDolphins > scoreKoalas){
+    console.log("Dolphins win the trophy")
+}else if(scoreDolphins < scoreKoalas){
+    console.log("Koalas win the trophy")
+}else{
+    console.log("Both win the trophy")
+} 

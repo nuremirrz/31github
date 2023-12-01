437dd5e29070cb8f6c3249846ca8b1b6fcfde2a4
Emirlan <nur.emirlan.02@gmail.com>
2023-12-01 16:32:17 +0600

    01

---
+++ 01.12.2023/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('i can drive')
+
+// const interface = 'audio'
+// const private = 534;
+// const if = 23;

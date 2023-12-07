39e359c14e43d52b503e1707f674a932514c5682
Emirlan <nur.emirlan.02@gmail.com>
2023-12-07 15:21:15 +0600

    the while loop

---
+++ 07.12.2023/index.js
@@ -0,0 +1,19 @@
+// the while loop
+
+// for(let rep = 1;rep <= 10; rep++) {
+//     console.log(`Lifting weights repetition ${rep}`)
+// }
+
+let rep = 1;
+while(rep <= 10) {
+    // console.log(`WHILE: Lifting weights repetition ${rep}`);
+    rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+    console.log(`you rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6 ) console.log('Loop is about ')
+}

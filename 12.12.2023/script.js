9a4dd47f8d2386ee0b28e04aa7baecf7cb02cac0
Emirlan <nur.emirlan.02@gmail.com>
2023-12-12 15:48:01 +0600

    Selecting and Manipulating Elements

---
+++ 12.12.2023/script.js
@@ -0,0 +1,11 @@
+'use strict'
+//  JavaScript in the Browser: DOM and Events Fundamentals
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 21;

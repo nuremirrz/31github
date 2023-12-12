5ee4d6a75e86c9e6860be52c209a229833fc9ceb
Emirlan <nur.emirlan.02@gmail.com>
2023-12-12 16:08:55 +0600

    Handling Click Events

--- 12.12.2023/script.js
+++ 12.12.2023/script.js
@@ -1,11 +1,22 @@
 'use strict'
 //  JavaScript in the Browser: DOM and Events Fundamentals
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number!';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number!';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
 
-console.log(document.querySelector('.guess').value);
-document.querySelector('.guess').value = 21;
+// console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 21;
+
+// Handling Click Events
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if(!guess) {
+        document.querySelector('.message').textContent = 'No Number'
+    }
+})

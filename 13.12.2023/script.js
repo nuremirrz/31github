dd0d342d96b561551166f9d57f13ed2eb19d83ca
Emirlan <nur.emirlan.02@gmail.com>
2023-12-13 15:26:56 +0600

    guess number

--- 13.12.2023/script.js
+++ 13.12.2023/script.js
@@ -14,16 +14,21 @@
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
-
+    // when ther is no input
     if(!guess) {
         document.querySelector('.message').textContent = 'No Number'
+    // when player win    
     }else if(guess === secretNumber) {
-        document.querySelector('.message').textContent = 'Correct Number!';        
+        document.querySelector('.message').textContent = 'Correct Number!';
+        document.querySelector('body').style.backgroundColor = '#60b347';        
+        document.querySelector('.number').style.width = '30rem';
+        document.querySelector('.number').textContent = secretNumber;
+    // when guess is too high            
     }else if(guess > secretNumber) {
         if(score > 1) {
             document.querySelector('.message').textContent = 'Too High';
@@ -33,6 +38,7 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.message').textContent = 'You lost the game';
             document.querySelector('.score').textContent = 0;
         }
+    // when guess is too low    
     }else if(guess < secretNumber) {
         if(score > 1) {
             document.querySelector('.message').textContent = 'Too Low';

10de0aeaaef1238cc0e332960f321ea516e05242
Emirlan <nur.emirlan.02@gmail.com>
2023-12-20 15:31:11 +0600

    scopping in practice

--- 20.12.2023/main.js
+++ 20.12.2023/main.js
@@ -1,10 +1,11 @@
+'use strict'
 // Scope and The Scope Chain
 
 // global scope
 
-const me = 'Emir';
-const job = 'student';
-const age = 21;
+// const me = 'Emir';
+// const job = 'student';
+// const age = 21;
 
 // function scope (local scope)
 
@@ -21,24 +22,64 @@ const age = 21;
 //     ///
 // }
 
-const myName = 'Emirlan';
+// const myName = 'Emirlan';
 
-function first() {
-    const age = 31;
+// function first() {
+//     const age = 31;
 
-    if (age >= 30) {
-        const decade = 3;
-        var millenial = true
-    }
+//     if (age >= 30) {
+//         const decade = 3;
+//         var millenial = true
+//     }
 
-    function second() {
-        const job = 'teacher';
-        console.log(`${myName} is a ${age}-old ${job}`)
-    }
+//     function second() {
+//         const job = 'teacher';
+//         console.log(`${myName} is a ${age}-old ${job}`)
+//     }
 
-    second()
-}
+//     second()
+// }
 
-first();
+// first();
 
 //variable lookup in scope chain
+
+// scopping in practice
+
+function calcAge(birthYear) {
+    const age = 2023 - birthYear;
+   
+    function printAge() {
+       let output = `${firstName}, You are ${age}, born in ${birthYear}`
+        console.log(output); 
+
+        if(birthYear >= 1981 && birthYear <= 1996) {
+            var millenial = true;
+
+            //creating NEW variable with same name as outer scope's variable
+            const firstName = 'Steven';
+            //reassigning outer scope's variable
+
+            output = 'NEW OUTPUT'
+
+            const str = `Oh, and  you are a millenial, ${firstName}`
+            console.log(str);
+
+            function add(a, b) {
+                return a + b;
+            }
+
+        }
+        // console.log(str);
+        console.log(millenial);
+        // console.log(add(2, 3)); 
+        console.log(output);        
+
+    }
+    printAge()
+
+    return age;
+}
+
+const firstName = ' Emirlan'
+calcAge(1992)

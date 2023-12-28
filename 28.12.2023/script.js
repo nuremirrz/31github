696e581dc0ee0c277c6e40937a712e224b06449a
Emirlan <nur.emirlan.02@gmail.com>
2023-12-28 23:37:08 +0600

    Looping Objects: Object Keys, Values, and Entries

---
+++ 28.12.2023/script.js
@@ -0,0 +1,74 @@
+'use strict';
+// Looping Objects: Object Keys, Values, and Entries
+
+const openingHours = {
+    thu: {
+        open: 12,
+        close: 22,
+    },
+    fri: {
+        open: 11,
+        close: 23,
+    },
+    sat: {
+        open: 0,
+        close: 24,
+    }
+}
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    
+    openingHours,
+
+    order(starterIndex, mainIndex) {
+         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+    },
+
+    orderDelivery({
+        starterIndex = 1, 
+        mainIndex = 0, 
+        time = '21:30', 
+        address
+    }) {
+        console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`)
+    },
+
+    orderPasta(ing1, ing2, ing3) {
+        console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`);
+    },
+
+    orderPizza(mainIngredient, ...otherIngredient) {
+        console.log(mainIngredient);
+        console.log(otherIngredient);
+    }
+};
+
+// Property Names
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of properties) {
+    openStr += `${day}, `;    
+}
+console.log(openStr);
+
+// Property Values
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire objects
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+
+// [key, value]
+for(const [key, { open, close }] of entries) {
+    console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
